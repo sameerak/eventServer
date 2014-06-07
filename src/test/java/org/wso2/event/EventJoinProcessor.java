5ad6a68db7ff386b8b067bee9c41e151a110c5d9
sameerak <dlskannangara@gmail.com>
2014-06-07 12:27:18 +0530

    adding updated join processor

--- src/test/java/org/wso2/event/EventJoinProcessor.java
+++ src/test/java/org/wso2/event/EventJoinProcessor.java
@@ -20,12 +20,20 @@ public class EventJoinProcessor {
     public static void main(String[] args) throws Exception {
 
         int port = 7614;
+        final int port1;
         String receiver = "localhost:7613";
         if (args.length != 0 && args[0] != null) {
             port = Integer.parseInt(args[0]);
         }
+
         if (args.length != 0 && args[1] != null) {
-            receiver = args[1];
+            port1 = Integer.parseInt(args[1]);
+        }
+        else
+        port1 = 7615;
+
+        if (args.length != 0 && args[2] != null) {
+            receiver = args[2];
         }
 
         StreamDefinition streamDefinition = new StreamDefinition();
@@ -35,12 +43,13 @@ public class EventJoinProcessor {
         streamDefinition.addAttribute("att3", StreamDefinition.Type.STRING);
         streamDefinition.addAttribute("att4", StreamDefinition.Type.INT);
 
-        StreamDefinition streamDefinition1 = new StreamDefinition();
+        final StreamDefinition streamDefinition1 = new StreamDefinition();
         streamDefinition1.setStreamId("TestStream1");
         streamDefinition1.addAttribute("att1", StreamDefinition.Type.INT);
         streamDefinition1.addAttribute("att2", StreamDefinition.Type.FLOAT);
         streamDefinition1.addAttribute("att3", StreamDefinition.Type.STRING);
-        streamDefinition1.addAttribute("att4", StreamDefinition.Type.INT);
+        streamDefinition1.addAttribute("att4", StreamDefinition.Type.FLOAT);
+        streamDefinition1.addAttribute("att5", StreamDefinition.Type.INT);
 
         final EventClient eventClient = new EventClient(receiver, streamDefinition); //creating a connection to the output receiver
 
@@ -60,10 +69,16 @@ public class EventJoinProcessor {
         siddhiManager.defineStream("define stream "+streamDefinition.getStreamId()+" ( "+attributeStr+" )");
         siddhiManager.defineStream("define stream "+streamDefinition1.getStreamId()+" ( "+attributeStr1+" )");
 
-        siddhiManager.addQuery("from TestStream#window.length(2000) join " +
-                "TestStream1#window.time(500) " +
-                "select * " +
+        siddhiManager.addQuery("from TestStream#window.length(2000)  as t join " +
+                "TestStream1#window.time(500) as n " +
+                "on t.att1 == n.att1 " +
+                "select t.att1, t.att2, t.att3, t.att4 " +
                 "insert into StockQuote;");
+
+//        siddhiManager.addQuery("from  TestStream1 [att1>50]" +
+//                "select att1, att2, att3, att5 "+
+//                "insert into StockQuote ;");
+
         siddhiManager.addCallback("StockQuote", new org.wso2.siddhi.core.stream.output.StreamCallback() {
             @Override
             public void receive(Event[] events) {
@@ -80,6 +95,40 @@ public class EventJoinProcessor {
                 }
             }
         });
+
+
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+        try {
+            EventServer eventServer1 = new EventServer(new EventServerConfig(port1), streamDefinition1, new org.wso2.event.server.StreamCallback() {
+                @Override
+                public void receive(Object[] event) {
+    //                System.out.println("test");
+                    InputHandler inputHandler = siddhiManager.getInputHandler("TestStream1");
+                    if(inputHandler != null) {
+                        try {
+                            inputHandler.send(event);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }else{
+                        System.out.println("Could not retrieve stream handler");
+                        throw new RuntimeException("Could not retrieve stream handler");
+                    }
+
+                }
+            });
+            eventServer1.start();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+            }
+        }).start();
 
         EventServer eventServer = new EventServer(new EventServerConfig(port), streamDefinition, new org.wso2.event.server.StreamCallback() {
             @Override
